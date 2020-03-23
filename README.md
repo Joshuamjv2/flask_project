5341ef69b5c56bc7dcd094d5a0acb0a207bd3b8e
Joshua <joshuamjv22@gmail.com>
2020-03-23 19:39:55 +0300

    smooth changes

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # flask_project
 teenCode2020 Andela Uganda, runtimeterror
 
-to run the program:
-1.Download the repository
-2.Open in IDE
-3.Open app.py
-4.runserver
+About
+=====
+This is a website that helps students who did A'Level apply for their desired course online.
+
+Technologies
+1.pipenv
+2.python 3.8.0
+3.Flask
+4.Bootstrap3
+5.jinja2
+
+to run the program in a local environment:
+1.Install pipenv
+2.Download the repository
+3.Open in IDE
+4.go to flask_project directory
+5.Start pip env
+6.Open app.py
+7.runserver
--- app.py
+++ app.py
@@ -1,20 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request
+# from Flask-WTF import Form
 app  = Flask(__name__)
-
-# subs = ['Sub-Math', 'ICT']
-# grading = {'A':6, 'B':5, 'C':4, 'D':3, 'E':2, 'O':1, 'F':0}
-
-# courses = ['Bsc. Engineering', 'Bsc. Medicine', 'Bsc. Law', 'Bsc. Computer Science', 'Bsc. Econ']
-# subjects_for_courses = {courses[0]:['Physics', 'Chemistry', 'Biology', 'Math'],
-#                                 courses[1]:['Physics', 'Chemistry', 'Biology', 'Math'],
-#                                 courses[2]:['History', 'Chemistry', 'Biology', 'Math', 'ICT', 'Geography', 'Literature', 'History'],
-#                                 courses[3]:['Math', 'Physics', 'Geography', 'Biology', 'ICT', 'Chemistry'],
-#                                 courses[4]:['Math', 'Physics', 'Geography', 'Biology', 'ICT', 'Chemistry']}
-# pscores = ['A', 'B', 'C', 'D', 'E', 'O', 'F']
-# weights_for_courses = {courses[0]:36, courses[1]:50, courses[2]:45, courses[3]:48, courses[4]:45}
-# course_subjects = []
-
-
 
 #print(courses[0])
 @app.route('/')
@@ -30,6 +16,7 @@ def home():
     pscores = ['A', 'B', 'C', 'D', 'E', 'O', 'F']
     grading = {'A':6, 'B':5, 'C':4, 'D':3, 'E':2, 'O':1, 'F':0}
     weights_for_courses = {courses[0]:36, courses[1]:50, courses[2]:45, courses[3]:48, courses[4]:45}
+    _gender = ['Male', 'Female']
     course_choice = request.form.getlist('course_choice')
     # got = request.form.get('got')
 
@@ -37,31 +24,31 @@ def home():
     # for i in subjects_for_courses:
     #     course_subjects = subjects_for_courses.get(course_choice)
         # return course_subjects
-
-
-    return render_template('home.html', subs = subs, course_choice = course_choice, courses = courses, subjects_for_courses = subjects_for_courses, pscores = pscores)
-
-
-
-
-
-
+    return render_template('home.html', subs = subs, _gender = _gender, course_choice = course_choice, courses = courses, subjects_for_courses = subjects_for_courses, pscores = pscores)
 
 
 
 @app.route('/result', methods = ['POST', 'GET'])
 def result():
+    courses = ['Bsc. Engineering', 'Bsc. Medicine', 'Bsc. Law', 'Bsc. Computer Science', 'Bsc. Econ']
+    subjects_for_courses = {courses[0]:['Physics', 'Chemistry', 'Biology', 'Math'],
+                                courses[1]:['Physics', 'Chemistry', 'Biology', 'Math'],
+                                courses[2]:['History', 'Chemistry', 'Biology', 'Math', 'ICT', 'Geography', 'Literature', 'History'],
+                                courses[3]:['Math', 'Physics', 'Geography', 'Biology', 'ICT', 'Chemistry'],
+                                courses[4]:['Math', 'Physics', 'Geography', 'Biology', 'ICT', 'Chemistry']}
 
-
-    principal1= request.form.get('principal_1')
-    principal2 = request.form.get('principal_2')
-    principal3 = request.form.get('principal_3')
+    course_choice = request.form.get('course_choice')
+    p1score = request.form.get('p1score')
+    name = request.form.get('name')
     subsidiary = request.form.get('subsidiary')
-    gp = request.form.get('general_paper')
+    gender = request.form.get('gender')
+    # principal3 = request.form.get('principal_3')
+    # subsidiary = request.form.get('subsidiary')
+    gp = request.form.get('gp')
     # final = {principal_1:'principal_1', principal_2:'principal_2', principal_3:'principal_3', subsidiary:'subsidiary', general_paper:'general_paper'}
     #ps = {principal_1:'principal_1'}
-    # return render_template('result.html', form = 'form')
-    return render_template('result.html', subsidiary = subsidiary)# 'principal_1 = 'principal_1')
+    # return render_template('result.html', form  = 'form')
+    return render_template('result.html', gp = gp, gender=gender, subsidiary=subsidiary, p1score = p1score, name = name, course_choice = course_choice, courses = courses, subjects_for_courses = subjects_for_courses)# 'principal_1 = 'principal_1')
 
 if __name__ == '__main__':
     app.run(debug=True)
